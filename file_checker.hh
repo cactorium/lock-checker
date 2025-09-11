88aa65e8934be292bd079038695d5975577ca340
Kelvin Ly <kelvin.ly1618@gmail.com>
2025-09-11 07:53:02 -0400

    Basic unit tests; seems to be working

--- file_checker.hh
+++ file_checker.hh
@@ -2,15 +2,56 @@
 
 #include <unordered_map>
 #include <unordered_set>
+#include <string>
 
 #include "func_walker.hh"
 
 namespace lock_checker {
 
-struct errors {
+struct error {
+    enum {
+        kDoubleTake, // two takes in a row
+        kGiveWithoutTake, // giving without taking first
+        kTakeWithoutGive, // missing give before return
+        kCallWithBlockingLock // a take followed by a blocking take in a function call
+    } typ;
 
-    void merge(const errors& other) {
-        // TODO
+    std::string extra;
+};
+
+struct errors {
+    std::vector<error> errs;
+
+    static error double_lock(const std::string& extra) {
+        return error {
+            error::kDoubleTake,
+            extra
+        };
+    }
+    static error give_without_take(const std::string& extra) {
+        return error {
+            error::kGiveWithoutTake,
+            extra
+        };
+    }
+    static error take_without_give(const std::string& extra) {
+        return error {
+            error::kTakeWithoutGive,
+            extra
+        };
+    }
+    static error call_with_blocking_lock(const std::string& extra) {
+        return error {
+            error::kCallWithBlockingLock,
+            extra
+        };
+    }
+
+    void add(const error& err) {
+        errs.push_back(err);
+    }
+    void add(error&& err) {
+        errs.push_back(err);
     }
 };
 
@@ -33,49 +74,65 @@ template <typename T> struct file_checker {
     std::unordered_map<FHandle, std::vector<callsite<T>>> called_by;
 
     void process_function(FHandle name, func<T> && f, std::unordered_map<Location, errors>& line_errors) {
-
         functions[name] = f;
+        process_function_internal(name, line_errors);
+    }
+    void process_function(FHandle name, const func<T> & f, std::unordered_map<Location, errors>& line_errors) {
+        functions[name] = f;
+        process_function_internal(name, line_errors);
+    }
 
+    void process_function_internal(FHandle name, std::unordered_map<Location, errors>& line_errors) {
         func<T>& fun = functions[name];
         bool uses_blocking_lock = false;
-        std::unordered_set<callsite<T>> unique_calls;
+        std::unordered_set<std::pair<callsite<T>, FHandle>> unique_calls;
 
         fun.template explore<int>([&](edge_state<T, int>& es, const bb<T>& basic_block, const action<T>& a) {
             if (a.typ == kLock) {
                 uses_blocking_lock = true;
                 if (es.locked_by_task) {
-                    // TODO add an error; double lock
+                    // double lock
+                    line_errors[a.loc].add(errors::double_lock(""));
                 }
             } else if (a.typ == kUnlock) {
                 if (!es.locked_by_task) {
-                    // TODO add an error; unlock with a lock
+                    // unlock with a lock
+                    line_errors[a.loc].add(errors::give_without_take(""));
                 }
             } else if (a.typ == kCall) {
                 if (a.called_func.has_value()) {
                     if (auto it = functions.find(*a.called_func); it != functions.end()) {
                         if (es.locked_by_task && deadlock_with_lock[*a.called_func]) {
                             // TODO add an error
+                            line_errors[a.loc].add(errors::call_with_blocking_lock(""));
                         }
                     } else {
                         if (es.locked_by_task) {
-                            unique_calls.insert(callsite<T>{a.loc, es.locked_by_task});
+                            unique_calls.insert({callsite<T>{a.loc, es.locked_by_task}, *a.called_func});
                         }
                     }
                 }
             } else if (a.typ == kEnd) {
                 if (es.locked_by_task) {
-                    // TODO add an error; lock held at the end of the function
+                    // lock held at the end of the function
+                    line_errors[a.loc].add(errors::take_without_give(""));
                 }
             }
         }, 0);
 
         deadlock_with_lock[name] = uses_blocking_lock;
+        for (auto &call: unique_calls) {
+            auto [callsite, func_handle] = call;
+            called_by[func_handle].push_back(callsite);
+        }
+
 
         if (auto it = called_by.find(name); it != called_by.end()) {
             auto &calls = it->second;
             for (const auto& cs: calls) {
                 if (uses_blocking_lock && cs.lock_state) {
                     // TODO: add an error
+                    line_errors[cs.loc].add(errors::call_with_blocking_lock(""));
                 }
             }
             called_by.erase(it);
@@ -85,11 +142,12 @@ template <typename T> struct file_checker {
 
 }
 
-template <typename T> struct std::hash<lock_checker::callsite<T>> {
-    std::size_t operator()(const lock_checker::callsite<T>& c) const {
-        std::size_t a = std::hash<typename T::Location>{}(c.loc);
-        std::size_t b = std::hash<typename lock_checker::callsite<T>::lock_state_type>{}(c.lock_state);
+template <typename T, typename U> struct std::hash<std::pair<lock_checker::callsite<T>, U>> {
+    std::size_t operator()(const std::pair<lock_checker::callsite<T>, U>& c) const {
+        std::size_t a1 = std::hash<typename T::Location>{}(c.first.loc);
+        std::size_t a2 = std::hash<typename lock_checker::callsite<T>::lock_state_type>{}(c.first.lock_state);
+        std::size_t a3 = std::hash<U>{}(c.second);
 
-        return (a << 1) ^ b;
+        return a1 ^ (a2 << 1) ^ (a3 << 1);
     }
 };
--- test_file_checker.cc
+++ test_file_checker.cc
@@ -90,4 +90,445 @@ TEST(test_file_checker, test_basic) {
     ASSERT_EQ(line_errors.size(), 0);
 }
 
+TEST(test_file_checker, test_basic_blocking_call) {
+    auto foo = lock_checker::func<BasicAdapter> {
+        // foo() {
+        //      lock(portMAX_DELAY); // 1
+        //      if (a) {
+        //          unlock(a); // 2
+        //      } else {
+        //          f(); // 3
+        //          unlock(a);
+        //      }
+        //      lock(portMAX_DELAY); // 4
+        //      unlock();
+        // }
+        .bbs = {
+            { // 0
+                .actions = {},
+                .loc = 0,
+                .next = {
+                    {1},
+                }
+            },
+            { // 1
+                .actions = {
+                    {kLock, 1},
+                },
+                .loc=0,
+                .next = {
+                    {2}, {3}
+                }
+            },
+            { // 2
+                .actions = {
+                    {kUnlock, 2},
+                },
+                .loc=1,
+                .next = {
+                    {4}
+                }
+            },
+            { // 3
+                .actions = {
+                    {kCall, 3, std::nullopt, std::string("f")},
+                    {kUnlock, 4},
+                },
+                .loc=3,
+                .next = {
+                    {4}
+                }
+            },
+            { // 4
+                .actions = {
+                    {kLock, 5},
+                    {kUnlock, 6},
+                },
+                .loc=5,
+                .next = {
+                    {5}
+                }
+            },
+            { // 5
+            },
+        },
+        .start_bb = {0},
+        .end_bb = {5},
+    };
+
+    // f() {
+    //      lock(portMAX_DELAY);
+    //      unlock();
+    // }
+    auto f = lock_checker::func<BasicAdapter> {
+        .bbs = {
+            { // 0
+                .actions = {},
+                .loc = 0,
+                .next = {
+                    {1},
+                }
+            },
+            { // 1
+                .actions = {
+                    {kLock, 1},
+                    {kUnlock, 1},
+                },
+                .loc=0,
+                .next = {
+                    {2},
+                }
+            },
+            { // 2
+            },
+        },
+        .start_bb = {0},
+        .end_bb = {2},
+    };
+
+    file_checker<BasicAdapter> fc;
+
+    std::unordered_map<int, errors> line_errors;
+
+    fc.process_function("f", std::move(f), line_errors);
+    ASSERT_EQ(line_errors.size(), 0);
+    fc.process_function("foo", std::move(foo), line_errors);
+    ASSERT_NE(line_errors.size(), 0);
+}
+
+TEST(test_file_checker, test_basic_blocking_call2) {
+    auto foo = lock_checker::func<BasicAdapter> {
+        // foo() {
+        //      lock(portMAX_DELAY); // 1
+        //      if (a) {
+        //          unlock(a); // 2
+        //      } else {
+        //          f(); // 3
+        //          unlock(a);
+        //      }
+        //      lock(portMAX_DELAY); // 4
+        //      unlock();
+        // }
+        .bbs = {
+            { // 0
+                .actions = {},
+                .loc = 0,
+                .next = {
+                    {1},
+                }
+            },
+            { // 1
+                .actions = {
+                    {kLock, 1},
+                },
+                .loc=0,
+                .next = {
+                    {2}, {3}
+                }
+            },
+            { // 2
+                .actions = {
+                    {kUnlock, 2},
+                },
+                .loc=1,
+                .next = {
+                    {4}
+                }
+            },
+            { // 3
+                .actions = {
+                    {kCall, 3, std::nullopt, std::string("f")},
+                    {kUnlock, 4},
+                },
+                .loc=3,
+                .next = {
+                    {4}
+                }
+            },
+            { // 4
+                .actions = {
+                    {kLock, 5},
+                    {kUnlock, 6},
+                },
+                .loc=5,
+                .next = {
+                    {5}
+                }
+            },
+            { // 5
+            },
+        },
+        .start_bb = {0},
+        .end_bb = {5},
+    };
+
+    // f() {
+    //      lock(portMAX_DELAY);
+    //      unlock();
+    // }
+    auto f = lock_checker::func<BasicAdapter> {
+        .bbs = {
+            { // 0
+                .actions = {},
+                .loc = 0,
+                .next = {
+                    {1},
+                }
+            },
+            { // 1
+                .actions = {
+                    {kLock, 1},
+                    {kUnlock, 1},
+                },
+                .loc=0,
+                .next = {
+                    {2},
+                }
+            },
+            { // 2
+            },
+        },
+        .start_bb = {0},
+        .end_bb = {2},
+    };
+
+    file_checker<BasicAdapter> fc;
+
+    std::unordered_map<int, errors> line_errors;
+
+    // swap the order around
+    fc.process_function("foo", std::move(foo), line_errors);
+    ASSERT_EQ(line_errors.size(), 0);
+    fc.process_function("f", std::move(f), line_errors);
+    ASSERT_NE(line_errors.size(), 0);
+}
+
+
+TEST(test_file_checker, test_missing_give) {
+    auto foo = lock_checker::func<BasicAdapter> {
+        // foo() {
+        //      lock(portMAX_DELAY); // 1
+        //      if (a) {
+        //          unlock(a); // 2
+        //      } else {
+        //          f(); // 3
+        //          //unlock(a); // missing unlock here
+        //      }
+        //      lock(portMAX_DELAY); // 4
+        //      unlock();
+        // }
+        .bbs = {
+            { // 0
+                .actions = {},
+                .loc = 0,
+                .next = {
+                    {1},
+                }
+            },
+            { // 1
+                .actions = {
+                    {kLock, 1},
+                },
+                .loc=0,
+                .next = {
+                    {2}, {3}
+                }
+            },
+            { // 2
+                .actions = {
+                    {kUnlock, 2},
+                },
+                .loc=1,
+                .next = {
+                    {4}
+                }
+            },
+            { // 3
+                .actions = {
+                    {kCall, 3, std::nullopt, std::string("f")},
+                    //{kUnlock, 4},
+                },
+                .loc=3,
+                .next = {
+                    {4}
+                }
+            },
+            { // 4
+                .actions = {
+                    {kLock, 5},
+                    {kUnlock, 6},
+                },
+                .loc=5,
+                .next = {
+                    {5}
+                }
+            },
+            { // 5
+            },
+        },
+        .start_bb = {0},
+        .end_bb = {5},
+    };
+
+    file_checker<BasicAdapter> fc;
+
+    std::unordered_map<int, errors> line_errors;
+
+    // swap the order around
+    fc.process_function("foo", std::move(foo), line_errors);
+    ASSERT_NE(line_errors.size(), 0);
+}
+
+TEST(test_file_checker, test_missing_give_return) {
+    auto foo = lock_checker::func<BasicAdapter> {
+        // foo() {
+        //      lock(portMAX_DELAY); // 1
+        //      if (a) {
+        //          unlock(a); // 2
+        //      } else {
+        //          f(); // 3
+        //          unlock(a); // missing unlock here
+        //      }
+        //      lock(portMAX_DELAY); // 4
+        //      //unlock();
+        // }
+        .bbs = {
+            { // 0
+                .actions = {},
+                .loc = 0,
+                .next = {
+                    {1},
+                }
+            },
+            { // 1
+                .actions = {
+                    {kLock, 1},
+                },
+                .loc=0,
+                .next = {
+                    {2}, {3}
+                }
+            },
+            { // 2
+                .actions = {
+                    {kUnlock, 2},
+                },
+                .loc=1,
+                .next = {
+                    {4}
+                }
+            },
+            { // 3
+                .actions = {
+                    {kCall, 3, std::nullopt, std::string("f")},
+                    //{kUnlock, 4},
+                },
+                .loc=3,
+                .next = {
+                    {4}
+                }
+            },
+            { // 4
+                .actions = {
+                    {kLock, 5},
+                    //{kUnlock, 6},
+                },
+                .loc=5,
+                .next = {
+                    {5}
+                }
+            },
+            { // 5
+            },
+        },
+        .start_bb = {0},
+        .end_bb = {5},
+    };
+
+    file_checker<BasicAdapter> fc;
+
+    std::unordered_map<int, errors> line_errors;
+
+    // swap the order around
+    fc.process_function("foo", std::move(foo), line_errors);
+    ASSERT_NE(line_errors.size(), 0);
+}
+
+
+TEST(test_file_checker, test_missing_take) {
+    auto foo = lock_checker::func<BasicAdapter> {
+        // foo() {
+        //      //lock(portMAX_DELAY); // 1
+        //      if (a) {
+        //          unlock(a); // 2
+        //      } else {
+        //          f(); // 3
+        //          unlock(a); // missing unlock here
+        //      }
+        //      lock(portMAX_DELAY); // 4
+        //      //unlock();
+        // }
+        .bbs = {
+            { // 0
+                .actions = {},
+                .loc = 0,
+                .next = {
+                    {1},
+                }
+            },
+            { // 1
+                .actions = {
+                    //{kLock, 1},
+                },
+                .loc=0,
+                .next = {
+                    {2}, {3}
+                }
+            },
+            { // 2
+                .actions = {
+                    {kUnlock, 2},
+                },
+                .loc=1,
+                .next = {
+                    {4}
+                }
+            },
+            { // 3
+                .actions = {
+                    {kCall, 3, std::nullopt, std::string("f")},
+                    //{kUnlock, 4},
+                },
+                .loc=3,
+                .next = {
+                    {4}
+                }
+            },
+            { // 4
+                .actions = {
+                    {kLock, 5},
+                    //{kUnlock, 6},
+                },
+                .loc=5,
+                .next = {
+                    {5}
+                }
+            },
+            { // 5
+            },
+        },
+        .start_bb = {0},
+        .end_bb = {5},
+    };
+
+    file_checker<BasicAdapter> fc;
+
+    std::unordered_map<int, errors> line_errors;
+
+    // swap the order around
+    fc.process_function("foo", std::move(foo), line_errors);
+    ASSERT_NE(line_errors.size(), 0);
+}
+
+
 }
